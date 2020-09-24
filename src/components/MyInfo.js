9109a5f1d9af08f7b658c36d88100245659a0526
Aditya.Gandi <aditya.gandi@mu-sigma.com>
2020-09-24 08:01:51 +0530

    first commit here

---
+++ src/components/MyInfo.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Brendan100</h1>
+        <p>
+          I am Brendan. I used to play pokemon planet, I was very good at it. But
+          I quit the game since I felt that the game is hindering my progress in
+          other aspects which are very necessary in my life.
+        </p>
+        <p>Below are top 3 vacation spots I would love to visit: </p>
+        <ul>
+          <li>1. Mountain top, maybe hiking</li>
+          <li>2. Beach without too much crowd, just to enjoy the breeze</li>
+          <li>3. An Oasis, maybe if the life permits</li>
+        </ul>
+      </div>
+    )
+  }
+
+export default MyInfo
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// importing components
+import MyInfo from "./components/MyInfo"
+
+ReactDOM.render(<MyInfo/>, document.getElementById("root"))
