04bb5c8c6a91f5e7f61391e5a450d3bd5599c5c7
Aditya.Gandi <aditya.gandi@mu-sigma.com>
2020-09-29 09:02:17 +0530

    learning how to use map to map components to data

--- src/components/JokeApp/JokeApp.js
+++ src/components/JokeApp/JokeApp.js
@@ -1,20 +1,32 @@
-import React from "react"
-import Joke from "./Joke"
+import React from "react";
+import Joke from "./Joke";
+import jokeData from "./JokeData";
 
 function JokeApp() {
-    return(
-        <div>
-            <Joke 
-            content={{question:"Why is 6 afraid of 7?",punchline:"Because 7 8 9"}}
-            />
-            <Joke 
-            content={{question:"Why do pandas like old films?", punchline:"Because they are black and white"}}
-            />
-            <Joke 
-            content={{punchline:"Boo"}}
-            />
-        </div>
-    )
+  const jokeComponents = jokeData.map((joke) => (
+    <Joke
+      key={joke.id}
+      content={{ question: joke.question, punchline: joke.punchline }}
+    />
+  ));
+  return (
+    <div>
+      {jokeComponents}
+      <Joke
+        content={{
+          question: "Why is 6 afraid of 7?",
+          punchline: "Because 7 8 9",
+        }}
+      />
+      <Joke
+        content={{
+          question: "Why do pandas like old films?",
+          punchline: "Because they are black and white",
+        }}
+      />
+      <Joke content={{ punchline: "Boo" }} />
+    </div>
+  );
 }
 
-export default JokeApp
+export default JokeApp;
---
+++ src/components/JokeApp/JokeData.js
@@ -0,0 +1,18 @@
+const jokesData = [
+  {
+    id: 1,
+    punchline: "Sometimes, just punchline is enough",
+  },
+  {
+    id: 2,
+    question: "which movies does pandas like?",
+    punchline: "Old ones, because they are black and white",
+  },
+  {
+    id: 3,
+    question: "why is 6 scared of 7?",
+    punchline: "Because 7 8 9",
+  },
+];
+
+export default jokesData;
